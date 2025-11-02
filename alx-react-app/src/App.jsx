528528c7701d4eb19da018b7e1a2abaca108c492
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-02 20:57:16 +0100

    2. Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +14,11 @@ function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
