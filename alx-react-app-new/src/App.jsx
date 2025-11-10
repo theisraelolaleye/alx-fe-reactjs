548fdcda8a34c96732bcede616d65f6ff61c1830
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-10 13:55:10 +0100

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+    </>
+  )
+}
+
+export default App
