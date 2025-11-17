843b6c845ece71da68d0cecd510cff6fa8f409d9
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-17 19:06:48 +0100

    0. Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/components/RecipeList.jsx
@@ -0,0 +1,22 @@
+// RecipeList component
+import useRecipeStore  from '../store/useRecipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias vitae? Consequatur aperiam amet, ab laboriosam quod corrupti quia, alias porro sed, harum eos dolor numquam omnis qui itaque fuga!
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+export default RecipeList;
+
---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddRecipeForm from '../components/AddRecipe.jsx'
+import RecipeList from '../components/RecipeList'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
