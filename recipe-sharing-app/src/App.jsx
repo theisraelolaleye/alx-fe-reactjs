6a70bdc7d7762fc6deeaa955fa27544a1a3afdeb
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-17 19:27:28 +0100

    file renamed

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AddRecipeForm from '../components/AddRecipe.jsx'
+import AddRecipeForm from '../components/AddRecipeForm.jsx'
 import RecipeList from '../components/RecipeList'
 
 
