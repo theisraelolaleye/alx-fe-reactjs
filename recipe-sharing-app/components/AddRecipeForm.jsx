6d3d6c4c8c03bfac80f1d51033809d5c46199ffa
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-17 19:33:59 +0100

    recipeStore location changed

--- recipe-sharing-app/components/AddRecipeForm.jsx
+++ recipe-sharing-app/components/AddRecipeForm.jsx
@@ -1,6 +1,6 @@
 // AddRecipeForm component
 import { useState } from 'react';
-import useRecipeStore from '../store/recipeStore';
+import useRecipeStore from '../src/components/recipeStore';
 
 const AddRecipeForm = () => {
   const addRecipe = useRecipeStore(state => state.addRecipe);
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -6,7 +6,6 @@ const RecipeList = () => {
 
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, molestias vitae? Consequatur aperiam amet, ab laboriosam quod corrupti quia, alias porro sed, harum eos dolor numquam omnis qui itaque fuga!
       {recipes.map(recipe => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
