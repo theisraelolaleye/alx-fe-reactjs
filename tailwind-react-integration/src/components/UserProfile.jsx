f9616a66533293d8cee7a2f76271c94638aa1d50
theisraelolaleye <theisraelolaleye@gmail.com>
2025-12-01 14:03:58 +0100

    fix: adjust image styling in UserProfile component for better responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="bg-gray-100 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8  ">
 
-      <img src="https://via.placeholder.com/150" alt="User" className=" rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto " />
+      <img src="https://via.placeholder.com/150" alt="User" className=" rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto " />
       <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
       <p className=" text-gray-600 sm:text-sm md:text-base " >Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
